80ba6c53ca6fe647f158c33caf753eea218e16a5
Nooshin <2011dragonmails@gmail.com>
2023-11-24 16:12:40 +0330

    Fix issue for deploying on Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
-"use client";
-
 import type { Metadata } from "next";
 import { Roboto} from "next/font/google";
 import "./globals.css";
 
 import { PomodoroProvider } from "./context/pomo-context";
 
-// const inter = Inter({ subsets: ["latin"] });
 const roboto = Roboto({ weight:['400','500','700'] ,subsets:["latin"]});
-// const abel = Abel({weight:'400',subsets:['latin']})
 
 export const metadata: Metadata = {
   title: "Create Next App",
